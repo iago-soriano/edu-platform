6edb129003a0a087149a00087c82c88374c31657
Iago Soriano <iago.srm.is@gmail.com>
2023-11-06 21:06:19 -0300

    infra added

---
+++ infra/resources.tf
@@ -0,0 +1,14 @@
+module "vpc" {
+  source = "../network"
+  tags        = var.tags
+}
+
+module "db" {
+  source = "../db"
+
+  tags        = var.tags
+
+  vpc_id            = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnet_ids
+  db_password       = var.db_password
+}
