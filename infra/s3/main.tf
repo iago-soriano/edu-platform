54d4343e70f42611ac7cc145a8333c122ef1f45d
Iago Soriano <iago.srm.is@gmail.com>
2024-02-08 08:08:14 -0300

    bucket acl public read

--- infra/s3/main.tf
+++ infra/s3/main.tf
@@ -1,4 +1,29 @@
-
 resource "aws_s3_bucket" "activity-content" {
   bucket = "activity-content"
+}
+
+resource "aws_s3_bucket_ownership_controls" "main" {
+  bucket = aws_s3_bucket.activity-content.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket = aws_s3_bucket.activity-content.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "main" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.main,
+    aws_s3_bucket_public_access_block.main,
+  ]
+
+  bucket = aws_s3_bucket.activity-content.id
+  acl    = "public-read"
 }
